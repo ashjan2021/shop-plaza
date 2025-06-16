c47b7c2b15b56f672256e9252cd4dc5b2a5a8144
ashjan2021 <ashishjangid2021@gmail.com>
2025-06-16 11:50:41 +0530

    small changes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 const config = require("config");
 
-const dbgr = require("debug")("development:mongoose");
+// const dbgr = require("debug")("development:mongoose");
 
+// mongoose
+// .connect(`${config.get("MONGODB_URI")}/scatch`)
 mongoose
-.connect(`${config.get("MONGODB_URI")}/scatch`)
-.then(function(){
-    // console.log("connected to db.");
-    dbgr("connected to db.");
-
-})
-.catch(function(err){
-    // console.log(err);
-   dbgr(err);
-})
+  .connect(process.env.MONGODB_URI)
+  .then(function () {
+    console.log("connected to db.");
+    // dbgr("connected to db.");
+  })
+  .catch(function (err) {
+    console.log(err);
+    //    dbgr(err);
+  });
 
 module.exports = mongoose.connection;
